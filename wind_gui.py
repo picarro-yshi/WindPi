8ef4d440ea96f6f6204645d0bd838e71a335199d
Yilin Shi <yshi@SCL-YSHI-MBP.Corp.Picarro.Com>
2024-07-09 18:01:44 -0700

    fig1 on mac

--- wind_gui.py
+++ wind_gui.py
@@ -264,23 +264,47 @@ class Window(QWidget):
         pass
 
     def plot_wind(self):
-        print('plot', self.filename)
+        # print('plot', self.filename)
 
-        # # time series
-        # df = pd.read_csv('20240708_101504.csv')
-        # # print(df)
-        #
-        # wind_u = df["NS_speed"][:240]
-        # wind_v = df["WE_speed"][:240]
-        # time = df["epoch_time"][:240]
-        # WS = np.sqrt(wind_u ** 2 + wind_v ** 2)
-        #
+        # time series
+        self.figure1.clear()
+        ax1 = self.figure1.add_subplot(111)
+
+        data_path = os.path.join(DATA_PATH, self.filename + ".csv")
+        df = pd.read_csv(data_path)
+        wind_u = df["NS_speed"]
+        wind_v = df["WE_speed"]
+        time = df["epoch_time"]
+
+        if df.shape[0] > 240:
+            wind_u = wind_u[:240]
+            wind_v = wind_v[:240]
+            time = time[:240]
+
+        wind_speed = np.sqrt(wind_u ** 2 + wind_v ** 2)
+
         # plt.figure()
-        # plt.quiver(time, WS, wind_u, wind_v)
+        # plt.quiver(time, wind_speed, wind_u, wind_v)
         # plt.show()
-        #
-        #
-        # # windrose
+
+        ax1.quiver(time, wind_speed, wind_u, wind_v)
+        self.canvas1.draw()
+
+        # windrose
+        def wind_uv_to_dir(U, V):
+            """
+            Calculates the wind direction from the u and v component of wind.
+            Takes into account the wind direction coordinates is different than the
+            trig unit circle coordinate. If the wind directin is 360 then returns zero
+            (by %360)
+            Inputs:
+              U = west/east direction (wind from the west is positive, from the east is negative)
+              V = south/noth direction (wind from the south is positive, from the north is negative)
+            """
+            WDIR = (270 - np.rad2deg(np.arctan2(V, U))) % 360
+            return WDIR
+
+        wind_dir = wind_uv_to_dir(wind_u, wind_v)
 
 
 
